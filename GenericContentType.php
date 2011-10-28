f38ea031c929db14b4ff02a64880a80310cfb2a1
Tobias Florek <me@ibotty.net>
2011-10-28 14:45:20 +0200

    prettify string construction in GenericContentType

--- GenericContentType.php
+++ GenericContentType.php
@@ -64,7 +64,7 @@ abstract class GenericContentType {
 
         add_filter( "the_content",  array($this, "the_content") );
 
-        GenericRecord::make_generic_record_class("".$this->id."Record", "$this->id");
+        GenericRecord::make_generic_record_class("$this->id"."Record", "$this->id");
     }
 
     function the_content ($content) {
