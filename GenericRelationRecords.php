baefa86b3e89ca5b60066768e5c06227b1fdf5dc
Tobias Florek <me@ibotty.net>
2011-10-30 22:38:32 +0100

    do not simply return the id, return the record

--- GenericRelationRecords.php
+++ GenericRelationRecords.php
@@ -98,15 +98,21 @@ class GenericRelationRecords {
         // aggregate
         while ($row = mysql_fetch_assoc($dbres)) {
             $i = $row["relation_id"];
-            if (!isset($res[$i]))
+            if (!isset($res[$i])) {
+                $relationship_id = $this->db_relationslug !== '' ? $this->db_relationslug : $row["relationship_id"];
+                $types = explode('_', $relationship_id);
+                $one_type     = $types[0]."Record";
+                $another_type = $types[1]."Record";
+
                 $res[$i] = array(
-                    "record"          => $this->db_is_reverse ? $row["post_to_id"] : $row["post_from_id"],
-                    "other_record"    => $this->db_is_reverse ? $row["post_from_id"] : $row["post_to_id"],
+                    "record"          => new $one_type($this->db_is_reverse ? $row["post_to_id"] : $row["post_from_id"]),
+                    "other_record"    => new $another_type($this->db_is_reverse ? $row["post_from_id"] : $row["post_to_id"]),
                     // the following looks odd, but is correct:
                     // if db_relationslug is '', the relation cannot be reverse.
-                    "relationship_id" => $this->db_relationslug !== '' ? $this->db_relationslug : $row["relationship_id"],
+                    "relationship_id" => $relationship_id,
                     "meta"            => array()
                 );
+            }
             if (! empty($row["meta_value"]))
               array_push($res[$i]["meta"], array($row["meta_key"] => $row["meta_value"]));
         }
