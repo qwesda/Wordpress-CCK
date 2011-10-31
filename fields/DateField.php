41b4a141024c08d356a165d657a0d594153ec71c
Tobias Florek <me@ibotty.net>
2011-10-31 14:42:50 +0100

    fix <input /> (missing "/")

--- fields/DateField.php
+++ fields/DateField.php
@@ -16,7 +16,7 @@ class DateField extends GenericField {
      */
     function echo_field_core ($post_data = array ()) {
         ?><input type="text" name="<?php echo "wpc_$this->id" ?>" class="wpc_input wpc_input_date hide-if-js"
-            id="<?php echo "wpc_$this->id" ?>" value="<?php if ( !empty($post_data) ) echo $post_data[$this->id]; ?>">
+            id="<?php echo "wpc_$this->id" ?>" value="<?php if ( !empty($post_data) ) echo $post_data[$this->id]; ?>" />
         <label class="wpc_hint hide-if-js" for="<?php echo "wpc_$this->id" ?>"><?php echo $this->hint ?></label>
         <span class='wpc_input_date_date hide-if-no-js'>
             <span id="wpc_input_date_timestamp-<?php echo $this->id;?>"> <?php if ($post_data[$this->id] !== '')
