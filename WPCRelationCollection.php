eb26f2a6687d9629819ce552de400c6162e54a08
Tobias Florek <me@ibotty.net>
2012-01-12 13:44:07 +0100

    initialize db_is_reverse

--- WPCRelationCollection.php
+++ WPCRelationCollection.php
@@ -35,6 +35,8 @@ class WPCRelationCollection extends WPCCollection {
         global $wpc_relationships;
 
         $db_relationslug = $type."_".$othertype;
+        $db_is_reverse = false;
+
         if (! isset($wpc_relationships[$db_relationslug])) {
             $db_relationslug = $othertype."_".$type;
             $db_is_reverse = true;
