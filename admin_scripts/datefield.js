57c1f291d821a2cd25e4f5a95ca9d4785cd06cec
Tobias Florek <me@ibotty.net>
2012-06-02 19:22:21 +0200

    add missing semicolon

--- admin_scripts/datefield.js
+++ admin_scripts/datefield.js
@@ -1,33 +1,29 @@
-
-
 (function($) {
     jQuery(document).ready(function (){
         jQuery('a.wpc_input_date_edit_link').click(function(event) {
             event.preventDefault();
-			
+
             jQuery(this).parent().hide();
             jQuery(this).parent().next().show();
         });
         jQuery('a.wpc_input_date_edit_ok').click(function(event) {
             event.preventDefault();
-        			
-			ok(this);
+            ok(this);
         });
         jQuery('.wpc_input_date_edit_container > input').keypress(function(event) {
-		     if(event.which == 13){
-        		 event.preventDefault();
-			 }
+            if(event.which == 13){
+                event.preventDefault();
+            }
         });
         jQuery('.wpc_input_date_edit_container > input').keyup(function(event) {
-		     if(event.which == 13){
-        		 event.preventDefault();
-				 
-				 ok(this);
-			 }
+            if(event.which == 13){
+                event.preventDefault();
+                ok(this);
+            }
         });
         jQuery('a.wpc_input_date_edit_cancel').click(function(event) {
             event.preventDefault();
-			
+
             jQuery(this).parent().prev().show();
             jQuery(this).parent().hide();
         });
@@ -37,26 +33,25 @@
 // mysqldate is from mike boone
 // http://boonedocks.net/mike/archives/157-Formatting-a-Javascript-Date-for-MySQL.html
 function mysqldate(date1) {
-  return date1.getFullYear() + '-' +
-    (date1.getMonth() < 9 ? '0' : '') + (date1.getMonth()+1) + '-' +
-    (date1.getDate() < 10 ? '0' : '') + date1.getDate();
+    return date1.getFullYear() + '-' +
+        (date1.getMonth() < 9 ? '0' : '') + (date1.getMonth()+1) + '-' +
+        (date1.getDate() < 10 ? '0' : '') + date1.getDate();
 }
 
 function ok (context) {
-	var id = jQuery(context).attr('id').replace(/[^\-]+\-/, "")
-			
+    var id = jQuery(context).attr('id').replace(/[^\-]+\-/, "");
+
     // the following selectors do not seem to work...
-    
-	var d = jQuery("#wpc_input_date_d-"+id).val();
+    var d = jQuery("#wpc_input_date_d-"+id).val();
     var m = jQuery("#wpc_input_date_m-"+id).val();
     var Y = jQuery("#wpc_input_date_y-"+id).val();
-			
-	m = (m < 10 ? '0' : '') + m;
-	d = (d < 10 ? '0' : '') + d;
-			
+
+    m = (m < 10 ? '0' : '') + m;
+    d = (d < 10 ? '0' : '') + d;
+
     jQuery("input[name='wpc_"+id+"']").val(Y+'-'+m+'-'+d);
     jQuery("#wpc_input_date_timestamp-"+id).html(Y+'-'+m+'-'+d);
 
     jQuery(context).parent().prev().show();
-    jQuery(context).parent().hide();	
+    jQuery(context).parent().hide();
 }
