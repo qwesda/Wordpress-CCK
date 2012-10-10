c7a079158a845e6117193d1d52e1081d72c8ea34
qwesda <qwesda@me.com>
2012-10-10 03:11:48 +0200

    post merge

--- fields/CheckBoxField.php
+++ fields/CheckBoxField.php
@@ -11,7 +11,7 @@ class CheckBoxField extends GenericField {
 
     function echo_field_core ($post_data = array ()) {
         $record = the_record();
-        $value  = $record->__get($this->id);
+        $value  = $record->get($this->id);
     ?>
         <input type="checkbox" class="wpc_input wpc_input_checkbox" name="<?php echo "wpc_$this->id" ?>" id="<?php echo "wpc_field_$this->id" ?>" value="1" <?php if ( !empty($value) ) echo "checked=\"checked\""; ?> /> <label for="<?php echo "wpc_field_$this->id" ?>"><?php echo $this->label ?></label>
     <?php }
