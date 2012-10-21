990c20f18d53087a3255a0cf088c1ee1613d8611
qwesda <qwesda@me.com>
2012-10-22 01:55:45 +0200

    added valuable comment

--- fields/GeneratedValue.php
+++ fields/GeneratedValue.php
@@ -5,6 +5,9 @@
  */
 
 class GeneratedValue extends FormattedString {
+    /*  das muss jetzt nur noch >entwerder< in die meta-db geschrieben werden
+        oder in die wp_post tabelle falls es post_title überschriebt ... bei den restlichen feldern macht das glaube ich weniger sinn */
+
     function __construct ($parent, $params, $callback) {
         parent::__construct ($parent, $params, $callback);
     }
