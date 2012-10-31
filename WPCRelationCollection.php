7433cb4a5ebaa1df6d72d241ffdce50cb373714a
Tobias Florek <me@ibotty.net>
2012-10-31 01:31:04 +0100

    implement RelationCollection

--- WPCRelationCollection.php
+++ WPCRelationCollection.php
@@ -6,15 +6,8 @@ require_once('WPCRelation.php');
  * relations records
  */
 class WPCRelationCollection extends WPCCollection {
-    protected $table        = "wp_wpc_relations";
-    protected $table_pk     = "relation_id";
-    protected $table_cols   = array(
-        "post_to_id",
-        "post_from_id",
-        "relationship_id"
-    );
-    protected $meta_table   = "wp_wpc_relations_meta";
-    protected $meta_fk      = "relation_id";
+    protected $table_pk = "id";
+    protected $table_cols = array('id', 'post_from_id', 'post_to_id');
 
     /**
      * the relationship's name in the db
@@ -40,19 +33,19 @@ class WPCRelationCollection extends WPCCollection {
         }
 
         $db_reverse = $reverse ? "true" : "false";
+        $table = $wpc_relationships[$typeslug]->table;
 
         $classname = ($reverse ? "Reverse" : "").str_replace(" ", "", ucwords(str_replace("_", " ", $typeslug)))."RelationRecords";
         if (! class_exists($classname)){
             $classdef = "class $classname extends ".__CLASS__." {
               protected \$typeslug = '$typeslug';
               protected \$db_is_reverse = $db_reverse;
+              protected \$table = '$table';
             }";
             eval ($classdef);
         }
 
         $relations = new $classname();
-
-        ButterLog::debug("relations_by_id($typeslug, $reverse, $id=null)");
 
         if ( !empty($id) ) {
             $relations = $relations->id_is($id);
@@ -62,17 +55,18 @@ class WPCRelationCollection extends WPCCollection {
     }
 
     function __construct() {
-        if (isset($this->typeslug))
-            $this->add_filter_('relationship_id', $this->typeslug);
+        global $wpc_relationships;
+
+        // add metafields
+        $this->table_cols = array_unique(array_merge($this->table_cols,
+            array_keys($wpc_relationships[$this->typeslug]->fields)));
     }
 
     /**
      * returns all filtered relations as array of WPCRelation objects.
      */
     function results() {
-        $res = array_map(array($this, "row_to_relation"), parent::results());
-
-        return $res;
+        return array_map(array($this, "row_to_relation"), parent::results());
     }
 
 
@@ -98,23 +92,16 @@ class WPCRelationCollection extends WPCCollection {
         return $ret;
     }
 
-
-
-
-
     function row_to_relation ($record) {
         global $wpc_relationships;
 
-        $row = $record[$this->table];
-
-        $relationship_id = $row["relationship_id"];
-
+        $row = $record['t'];
         return WPCRelation::new_relation(
-            $row["relation_id"],
-            $row["post_from_id"],
-            $row["post_to_id"],
-            $relationship_id,
-            $record["meta"]
+            $record['id'],
+            $row['post_from_id'],
+            $row['post_to_id'],
+            $this->typeslug,
+            $record['m']
         );
     }
 
@@ -123,7 +110,7 @@ class WPCRelationCollection extends WPCCollection {
      * returns a new instance.
      */
     function id_is($id) {
-        return $this->filter("id", $id);
+        return $this->filter('post_from_id', $id);
     }
 
     /**
@@ -131,16 +118,16 @@ class WPCRelationCollection extends WPCCollection {
      * returns a new instance.
      */
     function other_id_is($id) {
-        return $this->filter("other_id", $id);
+        return $this->filter("post_to_id", $id);
     }
 
     /**
      * this performs some magic to (re)order from and to to the expected order.
      */
     function add_filter_($key, $val, $op="=") {
-        if ($key == "id")
+        if ($key == "post_from_id")
             $key = $this->db_is_reverse ? "post_to_id" : "post_from_id";
-        else if ($key == "other_id")
+        else if ($key == "post_to_id")
             $key = $this->db_is_reverse ? "post_from_id" : "post_to_id";
 
         parent::add_filter_($key, $val, $op);
