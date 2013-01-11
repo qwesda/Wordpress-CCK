39402cdf208f0030a6a0a593995cfe6d6e0ec946
Tobias Florek <me@ibotty.net>
2013-01-11 15:13:14 +0100

    patch wp to support better search

---
+++ core-patches/PATCHES.mk
@@ -0,0 +1,10 @@
+7394-4.1.patch
+--------
+
+From Bug 7394 (Search: order results by relevance) 7394-4.patch adapted to
+work with WP 3.5.
+
+https://core.trac.wordpress.org/attachment/ticket/7394/7394-4.patch
+	
+
+
